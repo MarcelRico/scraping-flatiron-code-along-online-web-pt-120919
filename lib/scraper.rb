ba8b4f57697fa96846ba927bd9fdcfebc17d9304
Marcel Rico <personal.mr4@gmail.com>
2020-01-12 20:00:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,11 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    doc.css(".post").each do |post|
+      
+    end
+    
   end
   
   def get_courses
